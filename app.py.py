a6dce4eaa6b746e624714086a6e28348f9683502
SMMojtabaei <smmojtabaei@yahoo.com>
2025-06-17 20:11:32 +0100

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -1,10 +1,10 @@
 
 # Paste the Flask application code here
-
 from flask import Flask, render_template, request
 import numpy as np
 import tensorflow as tf
 import joblib
+import os
 
 app = Flask(__name__)
 
@@ -33,5 +33,7 @@ def index():
             prediction = "Error in input. Please check values."
 
     return render_template('index.html', prediction=prediction)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
